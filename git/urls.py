4d475a2153a67e04dd9223e4b35b49eeaaf8fe82
sreenu <sreenu.bojjireddy@gmail.com>
2021-06-20 22:45:21 +0530

    git project

--- git/urls.py
+++ git/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns=[
     path('',views.home,name='home'),
     path('list/',views.repo_list,name='list'),
+    path('<name>/details/',views.details,name='details'),
+    path('<branch>/files/',views.details,name='details'),
 ]
--- git/views.py
+++ git/views.py
@@ -1,23 +1,62 @@
+""" views for github project"""
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from github import Github
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
+from github.ContentFile import ContentFile
+
 
 def home(request):
     """For home page"""
     return render(request, 'git/index.html', {})
 
 
+open_git = ""
+user_name = ''
+
+
 def repo_list(request):
     """To display all repositories in git"""
+    # import pdb
+
     if request.method == "POST":
-        git = Github()
-        name = request.POST['username']
-        user = git.get_user(name)
-        print(user)
-        r = user.get_repos()
-        total = []
-        for each in r:
-            total.append(each.name)
-        return render(request, 'git/rep_list.html', {"list": total, "name": name})
+        global open_git
+        open_git = Github(request.POST["token"])
+        global user_name
+        user_name = request.POST['username']
+        user = open_git.get_user(user_name).get_repos()
+        print(dir(open_git))
+        list = []
+        for each in user:
+            list.append(each)
+        return render(request, 'git/rep_list.html', {"list": list, "name": user_name})
+
+
+def details(request, name):
+    """ detailsof repository and branches of repository"""
+    # user = open_git.get_user('sreenu0305')
+    print(user_name)
+    print('hello')
+    print(type(open_git))
+    repo = open_git.get_repo("{}/{}".format(user_name, name))
+    branch = list(repo.get_branches())
+    print(branch)
+    # import pdb
+    # pdb.set_trace()
+
+    return render(request, 'git/branch.html', {'branch': branch})
+
+
+def files(request,branch):
+    repo = open_git.get_repo("{}/{}".format(user_name, branch))
+    contents = repo.get_contents("")
+    for content_file in contents:
+
+        print(content_file)
+    ContentFile(path=".gitignore")
+    ContentFile(path="README.md")
+    ContentFile(path="sample.yaml")
+    return render(request, 'git/files.html')
