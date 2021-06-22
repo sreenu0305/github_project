b08fe9612c5809f09f0e49cbe4ac992557996821
sreenu <sreenu.bojjireddy@gmail.com>
2021-06-22 07:11:29 +0530

    added some functionalities

--- git/urls.py
+++ git/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('list/',views.repo_list,name='list'),
     path('<name>/details/',views.details,name='details'),
-    path('<branch>/files/',views.details,name='details'),
+    path('<name>/files/',views.details,name='details'),
+    path('<name>/create_branch/',views.create_branch,name='create_branch'),
 ]
--- git/views.py
+++ git/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from github import Github
 from django.http import HttpResponse, HttpResponseRedirect
 
-
 # Create your views here.
 from github.ContentFile import ContentFile
 
@@ -44,19 +43,47 @@ def details(request, name):
     repo = open_git.get_repo("{}/{}".format(user_name, name))
     branch = list(repo.get_branches())
     print(branch)
+    l = []
+    contents = repo.get_contents("")
+    # for content_file in contents:
+    #     contents = repo.get_contents("")
+    while contents:
+
+        file_content = contents.pop(0)
+
+        if file_content.type == "dir":
+
+            l.extend(repo.get_contents(file_content.path))
+        else:
+            l.append(file_content)
+        print(contents)
+        # l.append(content_file)
+        # print(content_file)
     # import pdb
     # pdb.set_trace()
 
-    return render(request, 'git/branch.html', {'branch': branch})
+    return render(request, 'git/branch.html', {'branch': branch, 'list': l, 'repo': repo})
 
 
-def files(request,branch):
-    repo = open_git.get_repo("{}/{}".format(user_name, branch))
+def files(request, name):
+    """ displaying files in branches"""
+    repo = open_git.get_repo("{}/{}".format(user_name, name))
     contents = repo.get_contents("")
     for content_file in contents:
-
         print(content_file)
-    ContentFile(path=".gitignore")
-    ContentFile(path="README.md")
-    ContentFile(path="sample.yaml")
+    # ContentFile(path=".gitignore")
+    # ContentFile(path="README.md")
+    # ContentFile(path="sample.yaml")
     return render(request, 'git/files.html')
+
+
+def create_branch(request, name):
+    """ create new branch """
+    repoName = name
+    source_branch = 'master'
+    target_branch = 'sreenu'
+
+    repo = open_git.get_user().get_repo(repoName)
+    sb = repo.get_branch(source_branch)
+    repo.create_git_ref(ref='refs/heads/' + target_branch, sha=sb.commit.sha)
+    return render(request,'git/create_branch.html',{})
